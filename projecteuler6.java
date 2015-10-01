c0b93e54433b0e3e528f12fc781fd61c06b5fbd7
Jim Kasper <jimkasper@Jims-MacBook-Pro.local>
2015-09-30 21:31:56 -0400

    Working

--- projecteuler6.java
+++ projecteuler6.java
@@ -1,12 +1,31 @@
-public class projecteuler5 {
+public class projecteuler6 {
 	public static void main(String[] args) {
 		
-		
-		int a = 1;
+	int square = 0;
+	int sumS = 0;
 	
-		
-		for(int sumOfSquare = a*a; a <= 100; a++){
-		}
+// sum of the squares		
+	int a = 0;
+	for(int sum = a*a; a < 101; a++){
+			sumS = a*a + sum + sumS;
+	}	
 	
+// square of the sum
+	int b = 0;
+	for(int total = b; b < 101; b++){
+			total = total + b;
+			square = total*total;
+	}	
+	
+// difference
+int difference = square - sumS;
+		
+		
+		System.out.println("Square of the sums: "+square);
+		System.out.println("Sum of the squares: "+sumS);	
+		System.out.println("Difference: "+difference);
+		
+		
+		
 	}
 }
